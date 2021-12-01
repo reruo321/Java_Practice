a99aa8de8faf4edf1d3ed7ca615c121fc91465e9
reruo321 <48712088+reruo321@users.noreply.github.com>
2021-12-02 01:34:06 +0900

    Update C05E04Scope.java

--- Chapter-05/C05E04Scope.java
+++ Chapter-05/C05E04Scope.java
@@ -1,5 +1,5 @@
 public class C05E04Scope {
-    int a = 10;
+    int a;
     public void oneMethod(int b){
         int a = 30;
         System.out.println("oneMethod a : " + a + ", parameter b : " + b);
