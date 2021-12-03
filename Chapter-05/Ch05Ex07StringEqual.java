1c68eb783038fbc78b7f84508c7ca661b5c6d6e5
reruo321 <48712088+reruo321@users.noreply.github.com>
2021-12-04 01:47:07 +0900

    Update Ch05Ex07StringEqual.java

--- Chapter-05/Ch05Ex07StringEqual.java
+++ Chapter-05/Ch05Ex07StringEqual.java
@@ -10,7 +10,7 @@ public class Ch05Ex07StringEqual {
 
         String stra = "Wow!";
         String strb = "Wow!";
-        System.out.println("= 연산자 변수 비교");
+        System.out.println("== 연산자 변수 비교");
         System.out.println("참조변수 비교: " + (stra == strb));
         System.out.println("참조내용 비교: " + (stra.equals(strb)));
         System.out.println("해쉬코드 stra: " + System.identityHashCode(stra) + ", strb: " + System.identityHashCode(strb));
